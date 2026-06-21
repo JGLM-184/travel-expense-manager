f86bb0db0aed13ab8a27c2b7960ca6c369245f25
João Gabriel <joaogabrielm591@gmail.com>
2026-06-21 16:01:56 -0300

    chore: initial commit with project structure and dependencies

---
+++ src/main/java/com/github/jglm_184/travel_expense_manager/TravelExpenseManagerApplication.java
@@ -0,0 +1,13 @@
+package com.github.jglm_184.travel_expense_manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelExpenseManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TravelExpenseManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/jglm_184/travel_expense_manager/TravelExpenseManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.jglm_184.travel_expense_manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravelExpenseManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
